a88bd2d25a07cff3f7d3ba220a2cb6312319df4b
lbrennaman <themasterarbiter01@gmail.com>
2023-03-08 16:08:24 -0500

    Here's the closest I could get to being able to translate text to blocks. The algorithm nearly works to translate a keystroke in a certain index of the Editor's UserInput... but a backspace shows just how far this algorithm is from being useful. It's taking so much code that I think I should look for some other way to do this, because it's just way too inefficient to be creating an algorithm with this many edge cases.

--- TestApp/src/WorkspaceHelper.jsx
+++ TestApp/src/WorkspaceHelper.jsx
@@ -1,6 +1,43 @@
 import Block from './Block';
 import UserInput from './UserInput';
 
+// arrayToString function: concatenate elements to form a single string
+export function arrayToString(array) {
+    var copy = "";
+    for (var i = 0; i < array.length; i++) {
+        copy += array[i];
+    }
+    return copy;
+}
+
+// subString function: return the list of characters in a string from indeces [start, end].
+export function subString(string, start, end) {
+    if (end > string.length - 1) {
+        console.log("Error: substring end is too big of an index for this string! Returning string...");
+        return string;
+    }
+
+    var copy = "";
+    for (var i = start; i <= end; i++) {
+        copy += string[i];
+    }
+    return copy;
+}
+
+// subArray function: get sub array of array from indeces [start, end]. Ex subArray([5, 9, 6, 7, 8], 0, 2) returns [5, 9, 6]
+export function subArray(array, start, end) {
+    if (end > array.length - 1) {
+        console.log("Error: subarray end is out of bounds! Returning array...");
+        return array;
+    }
+
+    var copy = [];
+    for (var i = start; i <= end; i++) {
+        copy.push(array[i]);
+    }
+    return copy;
+}
+
 // replaceArrayIndex function: replace a value in an array at the given index
 export function replaceArrayIndex(array, index, value) {
     var copy = [];
@@ -143,17 +180,14 @@ export function createEditorLines(list, index, updateBlockList, updateValue, upd
             );
         } else {
             // The type of data is a Block, so take list[i].value and concatenate the array into their string representation
-            var stringRepresentation = "";
-            for (var j = 0; j < list[i].value.length; j++) {
-                stringRepresentation += list[i].value[j];
-            }
+            var stringRepresentation = arrayToString(list[i].value);
             editorLines.push(
                 <div key={"Editor Line: " + i} className={"row p-0 m-0"} style={{height: '30px', width: '100%', border: '1px solid black'}}>
                     <UserInput 
                         value={stringRepresentation} 
                         index={i} 
                         updateValue={(value) => {
-                            console.log("Editor line block's updateValue fired!"); 
+                            console.log("Editor line block's updateValue fired! The retrieved value: ", value); 
                             
                             // Cannot updateValue like normal since a block's value is an array. UserInput stores a single string, so attempting to updateValue
                             // will override the array with a string.
@@ -161,6 +195,70 @@ export function createEditorLines(list, index, updateBlockList, updateValue, upd
                             // Options: 
                             // 1) do not provide the capability to create blocks from code
                             // 2) find a way to keep list[i].value an array and update list[i].value[j]
+
+                            // An algorithm that updates the proper list[i].value[j] must accomplish the following:
+                            // 1) The algorithm must not allow any indeces besides i and j to be altered
+                            // 2) Must be able to map indeces {[x, y]1, [x, y]2, ..., [x, y]n} -> {j1, j2, ..., jn}
+
+                            // Algorithm:
+                            // Loop through each character in value and copy each character into a new string while looping through j indeces using a counter k
+                            // If copy matches list[index].value[k][l], increment k
+                            // If copy does not match list[index].value[k][l], set list[index].value[k] to value
+
+                            //updateValue(replaceArrayIndex(list[index].value, index, value));
+                            var copyString = "";
+                            var copyArray = [];
+
+                            // split value into an array and see if it equals list[index].value
+                            var val_i = 0;
+                            for (var k = 0; k < list[index].value.length; k++) {
+                                if (list[index].value[k]) {
+                                    console.log("For ", list[index].value[k], " k = ", k, ": ", list[index].value[k], " exists.");
+                                    for (var l = 0; l < list[index].value[k].length; l++) {
+                                        if (list[index].value[k][l] == value[val_i]) {
+                                            console.log("For ", list[index].value[k], " k = ", k, " l = ", l, ": ", list[index].value[k][l], " == ", value[val_i]);
+                                            copyString += value[val_i];
+                                            val_i++;
+                                        }
+                                    }
+
+                                    if (copyString == list[index].value[k]) {
+                                        console.log("Made it through successfully. ", copyString, " == ", list[index].value[k]);
+                                        if (k == list[index].value.length - 1) {
+                                            if (value[val_i]) {
+                                                copyString += value[val_i];
+                                            }
+                                        }
+                                        copyArray.push(copyString);
+                                        copyString = "";
+                                    } else {
+                                        console.log("Somehow unsuccessful: ", copyString);
+                                    }
+                                } else {
+                                    console.log("For ", list[index].value[k], " k = ", k, ": ", list[index].value[k], " does not exist. Do nothing.");
+                                    if (list[index].value[k + 1]) {
+                                        if (list[index].value[k + 1][0] != value[val_i]) {
+                                            copyArray.push(value[val_i]);
+                                            copyString = "";
+                                            val_i++;
+                                        } else {
+                                            copyArray.push("");
+                                            copyString = "";
+                                        }
+                                    } else {
+                                        if (value[val_i]) {
+                                            copyArray.push(value[val_i]);
+                                            copyString = "";
+                                            val_i++;
+                                        } else {
+                                            copyArray.push("");
+                                            copyString = "";
+                                        }
+                                    }
+                                }
+                            }
+
+                            updateValue(copyArray);
                         }} 
                         updateIndex={updateIndex.i}
                         handleKeyDown={(event) => handleKeyDown(event, list, index, updateBlockList)}
