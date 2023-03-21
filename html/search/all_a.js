8df49ee0656042bc3113238ffc8b685ec582a167
lbrennaman <themasterarbiter01@gmail.com>
2023-03-21 19:12:09 -0400

    Attempt 3: making really sure doxygen directories are not pushed.

---
+++ html/search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['userinput_0',['UserInput',['../classUserInput.html',1,'']]],
+  ['userinputcontroller_1',['UserInputController',['../classUserInputController.html',1,'']]]
+];
---
+++ html/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['createdivider_0',['CreateDivider',['../classCreateDivider.html',1,'']]],
+  ['createform_1',['CreateForm',['../classCreateForm.html',1,'']]]
+];
