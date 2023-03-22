3c808385a015853311237eb2199204538a1faebb
lbrennaman <themasterarbiter01@gmail.com>
2023-03-22 17:13:38 -0400

    Testing doxygen 3

--- TestApp/src/main.jsx
+++ TestApp/src/main.jsx
@@ -16,7 +16,7 @@ import FileWriter from './FileWriter';
  *  Creates the MainView component and attaches it to the DOM via a div with the id "MainViewController."
  */
 
-/*!
+/*! @fn
  *  @brief MainView Component: controls the entire interface
  *
  *  The MainView Component represents the entire interface as it holds all subcomponents along with the data that needs to be
@@ -30,19 +30,46 @@ import FileWriter from './FileWriter';
 function MainView(properties) {
 
     // Define state variables via hooks
-    const [search, updateSearch] = useState("");                                        ///< DragZone current search bar input
-    const [dragZoneSelected, updateDragZoneSelected] = useState(null);                  ///< Current block selected in the DragZone
-    const [blockZoneSelected, updateBlockZoneSelected] = useState(null);                ///< Current line selected in the BlockZone
-    const [input, updateInput] = useState("");                                          ///< DragZone/BlockZone current textarea input of block in focus
-    const [blockValues, updateBlockValues] = useState(null);                            ///< Current list of blocks to display in the DragZone
-    const [bzValues, updateBZValues] = useState([{type: UserInput, value: ""}]);        ///< Current list of blocks/lines to display in the BlockZone
+    /*! @var 
+     *  @brief DragZone current search bar input
+     */
+    const [search, updateSearch] = useState("");  
 
-    /// The Workspace component holding the BlockZone and Editor
+    /*! @var 
+     *  @brief Current block selected in the DragZone
+     */
+    const [dragZoneSelected, updateDragZoneSelected] = useState(null);                 
+    
+    /*! @var 
+     *  @brief Current line selected in the BlockZone
+     */
+    const [blockZoneSelected, updateBlockZoneSelected] = useState(null);  
+    
+    /*! @var 
+     *  @brief DragZone/BlockZone current textarea input of block in focus
+     */
+    const [input, updateInput] = useState("");  
+    
+    /*! @var 
+     *  @brief Current list of blocks to display in the DragZone
+     */
+    const [blockValues, updateBlockValues] = useState(null);  
+    
+    /*! @var 
+     *  @brief Current list of blocks/lines to display in the BlockZone
+     */
+    const [bzValues, updateBZValues] = useState([{type: UserInput, value: ""}]); 
+
+    /*! @var 
+     *  @brief The Workspace component holding the BlockZone and Editor
+     */
     const [workspace, updateWorkspace] = useState(
         <Workspace blockList={bzValues} updateBlockList={updateBZValues} updateInput={updateInput} updateMainIndex={updateBlockZoneSelected}/>
     );
 
-    /// The FileWriter controlling the file writing process
+    /*! @var 
+     *  @brief The FileWriter controlling the file writing process
+     */
     const [f_writer, updateWriter] = useState(<FileWriter blockList={bzValues}/>);
 
     // When the BlockZone's blockList is updated, refresh the Workspace to show these changes
@@ -51,7 +78,9 @@ function MainView(properties) {
         updateWriter(<FileWriter blockList={bzValues}/>);
     }, [bzValues]);
 
-    /// The DragZone component holding the search/filter bar and the corresponding list of blocks to choose from
+    /*! @var 
+     *  @brief The DragZone component holding the search/filter bar and the corresponding list of blocks to choose from
+     */
     const [dragZone, updateDragZone] = useState(
         <DragZone 
         blockList={blockValues}
--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['main_2ejsx_0',['main.jsx',['../main_8jsx.html',1,'']]],
-  ['mainview_1',['MainView',['../main_8jsx.html#ac6f18d9378c4f1a9596c35cd39aa0fe4',1,'main.jsx']]]
+  ['main_2ejsx_0',['main.jsx',['../main_8jsx.html',1,'']]]
 ];
