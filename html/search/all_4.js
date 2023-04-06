fb3bfc21f3c3223fe5181bd4a36b8c7b77ca9b10
lbrennaman <themasterarbiter01@gmail.com>
2023-04-06 16:29:58 -0400

    Pushing again because I forgot to update doxygen documentation

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['filewriter_0',['FileWriter',['../main_8jsx.html#a7c123d7946afd66899229f7382329465',1,'FileWriter:&#160;main.jsx'],['../FileWriter_8jsx.html#a209ff2dc7ec6db2c7499fd8875caab4d',1,'FileWriter(properties):&#160;FileWriter.jsx']]],
-  ['filewriter_2ejsx_1',['FileWriter.jsx',['../FileWriter_8jsx.html',1,'']]]
+  ['filewriter_2ejsx_1',['FileWriter.jsx',['../FileWriter_8jsx.html',1,'']]],
+  ['formatstringasjson_2',['formatStringAsJSON',['../FileWriter_8jsx.html#a54a7c39ccf7f675fb79ed9307549ba63',1,'FileWriter.jsx']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['filewriter_0',['FileWriter',['../FileWriter_8jsx.html#a209ff2dc7ec6db2c7499fd8875caab4d',1,'FileWriter.jsx']]]
+  ['filewriter_0',['FileWriter',['../FileWriter_8jsx.html#a209ff2dc7ec6db2c7499fd8875caab4d',1,'FileWriter.jsx']]],
+  ['formatstringasjson_1',['formatStringAsJSON',['../FileWriter_8jsx.html#a54a7c39ccf7f675fb79ed9307549ba63',1,'FileWriter.jsx']]]
 ];
